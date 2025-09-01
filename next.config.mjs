7314aaa2ad32d3e86aba03ace003c9b3a947db75
Johnniel Mar <marjohnniel@gmail.com>
2025-09-01 22:20:30 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    nodeMiddleware: true, // 👈 enables Node runtime for middleware
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
